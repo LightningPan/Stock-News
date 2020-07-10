d87243f4f2dd4ce145f53266c80f09c9e21dee77
LightningPan <tp1032189322@live.com>
2020-07-10 14:37:55 +0800

    version 0.1.1 新增:   新闻查询   股票信息按指定时间跨度查询   股票信息按时间升序/降序查询 优化：   对查询产生的异常进行处理

--- Server/src/main/java/com/server/main/StockDetailController.java
+++ Server/src/main/java/com/server/main/StockDetailController.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,21 +18,60 @@ public class StockDetailController {
 
     @RequestMapping(value = "Day",method= RequestMethod.GET)
     public List<Object[]> searchByDate(@RequestParam("Date") String date,
-                                       @RequestParam("Location") String location,
+                                       @RequestParam(value = "Location",defaultValue = "sh") String location,
                                        @RequestParam("StockCode") String StockCode){
+        try{
         String sql="select * from "+location+StockCode+" where time='"+date+"'";
         Query nativeQuery=em.createNativeQuery(sql);
         List<Object[]> resultList=nativeQuery.getResultList();
-        return resultList;
+        return resultList;}
+        catch (Exception e){
+            List<Object[]> result=new ArrayList<Object[]>() ;
+            Object[] failResult=new Object[1];
+            failResult[0]="Fail";
+            result.add(failResult);
+            return result;
+        }
     }
 
     @RequestMapping(value = "Month",method = RequestMethod.GET)
     public List<Object[]> searchByMonth(@RequestParam("Date") String date,
-                                       @RequestParam("Location") String location,
-                                       @RequestParam("StockCode") String StockCode){
-        String sql="select * from "+location+StockCode+" where DATE_FORMAT(time,'%Y%m')='"+date+"'";
+                                       @RequestParam(value = "Location",defaultValue = "sh") String location,
+                                       @RequestParam("StockCode") String StockCode,
+                                        @RequestParam(value = "Order" ,defaultValue = "desc")String order){
+        try{
+        String sql="select * from "+location+StockCode+" where DATE_FORMAT(time,'%Y%m')='"+date+"' order by time "+order;
         Query nativeQuery=em.createNativeQuery(sql);
         List<Object[]> resultList=nativeQuery.getResultList();
         return resultList;
+        }
+        catch(Exception e){
+            List<Object[]> result=new ArrayList<Object[]>() ;
+            Object[] failResult=new Object[1];
+            failResult[0]="Fail";
+            result.add(failResult);
+            return result;
+        }
+    }
+
+    @RequestMapping(value="Range",method = RequestMethod.GET)
+    public List<Object[]> searchRange(@RequestParam("Date") String date,
+                                      @RequestParam(value = "Location",defaultValue = "sh") String location,
+                                      @RequestParam("StockCode") String StockCode,
+                                      @RequestParam(value = "Range" ,defaultValue = "10") Integer range,
+                                      @RequestParam(value="Order",defaultValue = "desc") String order){
+        try{
+        String sql="select * from "+location+StockCode+" where time>='"+date+"' order by time "+order+" limit "+range;
+        Query nativeQuery=em.createNativeQuery(sql);
+        List<Object[]> resultList=nativeQuery.getResultList();
+        return resultList;
+        }
+        catch (Exception e){
+            List<Object[]> result=new ArrayList<Object[]>() ;
+            Object[] failResult=new Object[1];
+            failResult[0]="Fail";
+            result.add(failResult);
+            return result;
+        }
     }
 }
--- Server/src/main/java/com/server/main/StockNewsController.java
+++ Server/src/main/java/com/server/main/StockNewsController.java
@@ -1,4 +1,41 @@
 package com.server.main;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "StockNews")
 public class StockNewsController {
+    @PersistenceContext
+    private EntityManager em;
+
+    @RequestMapping(value="Range",method = RequestMethod.GET)
+    public List<Object[]> searchRange(@RequestParam("Date") String date,
+                                      @RequestParam(value = "Location",defaultValue = "sh") String location,
+                                      @RequestParam("StockCode") String StockCode,
+                                      @RequestParam(value = "Range" ,defaultValue = "10") Integer range,
+                                      @RequestParam(value="Order",defaultValue = "desc") String order){
+        try{
+            String sql="select * from "+location+StockCode+" where releaseTime>='"+date+"' order by releaseTime "+order+" limit "+range;
+            Query nativeQuery=em.createNativeQuery(sql);
+            List<Object[]> resultList=nativeQuery.getResultList();
+            return resultList;
+        }
+        catch (Exception e){
+            List<Object[]> result=new ArrayList<Object[]>() ;
+            Object[] failResult=new Object[1];
+            failResult[0]="Fail";
+            result.add(failResult);
+            return result;
+        }
+    }
+
 }
